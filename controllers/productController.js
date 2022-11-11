941304dc38a9895c4399e26bac0156885d27c4ef
NAYEEM65 <mathnayeem054@gmail.com>
2022-11-11 18:49:11 +0600

    add new product done

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,12 @@
+const Product = require('../models/productModel');
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(200).json({
+        success: true,
+        product,
+    });
+};
+
 exports.getAllProducts = (req, res) => {
     res.status(200).json({ message: 'route is available' });
 };
