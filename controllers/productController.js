2ca36b66210ac28ea5958fbe6efe156a827c5bc0
NAYEEM65 <mathnayeem054@gmail.com>
2022-11-11 19:03:39 +0600

    update product done

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,27 @@
 const Product = require('../models/productModel');
 
-//admin model
+//create a new Product -- admin only
 exports.createProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product,
+    });
+};
+//update a new Product -- admin only
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        res.status(500).json({
+            success: false,
+            message: 'Product not found',
+        });
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
     res.status(201).json({
         success: true,
         product,
