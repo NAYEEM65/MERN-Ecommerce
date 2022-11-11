e3e0e8b82cdd4f89028ace2cacc2dd6ee113392b
NAYEEM65 <mathnayeem054@gmail.com>
2022-11-11 18:52:54 +0600

    get all products route done

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,15 @@
 const Product = require('../models/productModel');
+
+//admin model
 exports.createProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
-    res.status(200).json({
+    res.status(201).json({
         success: true,
         product,
     });
 };
-
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({ message: 'route is available' });
+//get All Products
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.status(200).json({ success: true, products });
 };
