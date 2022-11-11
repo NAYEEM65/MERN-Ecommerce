30b334d79f8f124db2309012873db9c28e9e8afe
NAYEEM65 <mathnayeem054@gmail.com>
2022-11-11 18:19:31 +0600

    server is connected to mongodb database

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const app = require('./app');
 const express = require('express');
 const dotenv = require('dotenv');
-
+const connectDB = require('./config/database');
+//config
 dotenv.config({ path: 'server/config/config.env' });
+
+//middleware
 app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
+
+//connectDB
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`server listening on ${PORT}`);
